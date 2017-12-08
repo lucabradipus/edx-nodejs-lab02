1419d5160308dfbc1263b965c89fdf6a3e7b98fc
Luca Terzaroli <lucabradipus@gmail.com>
2017-12-08 16:58:54 +0100

    test for posts CRU without D, for the moment

--- routes/posts.js
+++ routes/posts.js
@@ -1,21 +1,61 @@
 let store = require('../models/post')
+const payloadCheck = require('payload-validator');
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+const wellFormedPost = {
+  name: "",
+  url: "",
+  text: ""
+}
+
+function validatePayload(receivedPayload) {
+  const result = payloadCheck.validator(receivedPayload, wellFormedPost, ["name", "url"], true);
+  return result
+}
+
+function createPost(receivedPayload) {
+  let newPost = new Object({
+    name: receivedPayload.name, url: receivedPayload.url, text: receivedPayload.text
+  })
+
+  return newPost
+}
+
 module.exports = {
   getPosts(req, res) {
-    res.status(200).send(store.posts)
+    let data = store.posts
+    if (req.query.postId) {
+      const index = parseInt(req.query.postId)
+      if (isNumber(index)) {
+        data = store.posts[index]
+        if (!data) return res.status(404).send()
+      } else {
+        return res.status(400).send()
+      }
+    }
+    res.status(200).send(data)
   },
   addPost(req, res) {
-    let newPost = req.body
+    const validation = validatePayload(req.body)
+    if (!validation.success) return res.status(406).send(validation.response)
+    let newPost = createPost(req.body )
     let id = store.posts.length
     store.posts.push(newPost)
     res.status(201).send({id: id})
   },
   updatePost(req, res) {
-    store.posts[req.params.id] = req.body
+    const validation = validatePayload(req.body)
+    if (!validation.success) return res.status(406).send(validation.response)
+    let updatedPost = createPost(req.body )
+    store.posts[req.params.id] = updatedPost
     res.status(200).send(store.posts[req.params.id])
 
   },
   removePost(req, res) {
-    store.posts.splice(req.params.id,1)
+    store.posts.splice(req.params.id, 1)
     res.status(204).send()
 
   }
--- test/posts_test.js
+++ test/posts_test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../server').app;
 const server = require('../server').server;
 const expect = require('chai').expect
-
+let store = require('../models/post')
 
 after(function (done) {
   console.log('About to exit, waiting for remaining connections to complete');
@@ -10,46 +10,182 @@ after(function (done) {
   done()
 });
 
-describe('GET posts', function () {
+afterEach(function (done) {
+  store.posts = []
+  done()
+})
+
+function storePost() {
+  store.posts.push({
+      name: "Top 10 ES6 Features",
+      url: "http://webapplog.com/es6",
+      text: ""
+    }
+  )
+}
+
+describe('READ posts', function () {
   it('returns an empty array when called the first time', (done) => {
     request(app)
-        .get('/posts')
-        .end((err, res) => {
-          expect(res.status).to.equal(200);
-          expect(res.body.length).to.equal(0);
-          done();
-        })
+      .get('/posts')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.length).to.equal(0);
+        done();
+      })
   })
+  it('returns a single post', (done) => {
+    storePost();
+    request(app)
+      .get(`/posts?postId=0`)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.name).to.equal('Top 10 ES6 Features');
+        done();
+      })
+  })
+  it('checks wrong query parameters in GET', (done) => {
+    storePost();
+
+    request(app)
+      .get(`/posts?postId=foo`)
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        done();
+      })
+  })
+  it('checks out of index query parameters in GET', (done) => {
+    storePost();
+
+    request(app)
+      .get(`/posts?postId=1`)
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        done();
+      })
+  })
+
 })
 //
-describe('POST posts', function () {
+describe('CREATE posts', function () {
   it('creates an item ', (done) => {
-    request(app)
     let post = {
       name: "Top 10 ES6 Features",
       url: "http://webapplog.com/es6",
       text: ""
     }
     request(app)
-        .post('/posts')
-        .send(post)
-        .end((err, res) => {
-          expect(res.status).to.equal(201);
-          expect(res.body.id).to.equal(0);
-          if (err) done(err)
-          done();
-        });
-  })
-  it('check the created item ', (done) => {
+      .post('/posts')
+      .send(post)
+      .end((err, res) => {
+        expect(res.status).to.equal(201);
+        expect(res.body.id).to.equal(0);
+      });
     request(app)
-        .get('/posts')
-        .end((err, res) => {
-          expect(res.status).to.equal(200);
-          expect(res.body.length).to.equal(1);
-          expect(res.body[0].name).to.equal('Top 10 ES6 Features');
-          if (err) done(err)
-          done();
-        })
+      .get('/posts')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.length).to.equal(1);
+        expect(res.body[0].name).to.equal('Top 10 ES6 Features');
+        done();
+      })
 
   })
+  it('discards extra parameters in POST ', (done) => {
+    let post = {
+      name: "Top 10 ES6 Features",
+      url: "http://webapplog.com/es6",
+      text: "",
+      extra: "extra param"
+    }
+    request(app)
+      .post('/posts')
+      .send(post)
+      .end((err, res) => {
+        expect(res.status).to.equal(201);
+        expect(res.body.id).to.equal(0);
+      });
+    request(app)
+      .get('/posts')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.length).to.equal(1);
+        expect(res.body[0].extra).to.equal(undefined);
+        done();
+      })
+
+  })
+
+  it('DOES NOT create an item with malformed Json', (done) => {
+    request(app)
+    let post = {
+      name: "",
+      url: "http://webapplog.com/es6",
+      text: ""
+    }
+    request(app)
+      .post('/posts')
+      .send(post)
+      .end((err, res) => {
+        expect(res.status).to.equal(406);
+        expect(res.body.errorKey[0]).to.equal('name');
+        done()
+      });
+  })
+})
+
+describe('UPDATE posts', function () {
+  it('update an item ', (done) => {
+    storePost()
+    let update = {
+      name: "updated",
+      url: "http://webapplog.com/es6",
+      text: ""
+    }
+    request(app)
+      .put('/posts/0')
+      .send(update)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.name).to.equal("updated");
+        done();
+      })
+
+  })
+  it('discards extra params in json ', (done) => {
+    storePost()
+    let update = {
+      name: "updated",
+      url: "http://webapplog.com/es6",
+      text: "",
+      extra: "extra param"
+    }
+    request(app)
+      .put('/posts/0')
+      .send(update)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.extra).to.equal(undefined);
+        done();
+      })
+
+  })
+  it('DOES NOT accept invalid json ', (done) => {
+    storePost()
+    let update = {
+      name: "",
+      url: "http://webapplog.com/es6",
+      text: ""
+    }
+    request(app)
+      .put('/posts/0')
+      .send(update)
+      .end((err, res) => {
+        expect(res.status).to.equal(406);
+        expect(res.body.extra).to.equal(undefined);
+        done();
+      })
+
+  })
+
 })
