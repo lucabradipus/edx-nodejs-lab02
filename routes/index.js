f84ea11dea728e880a40c02c099ef54f3a274eb1
Luca Terzaroli <luca.terzaroli@opicino.it>
2017-12-11 10:02:12 +0100

    using a routes Middleware to manage routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,16 @@
 
 const posts = require('./posts')
 const comments = require('./comments')
-module.exports =  {comments: comments, posts: posts}
+const express = require('express')
+var router = express.Router();
+router.get('/posts', posts.getPosts);
+router.post('/posts', posts.addPost);
+router.put('/posts/:postId/', posts.updatePost);
+router.delete('/posts/:postId/', posts.removePost);
+//
+router.get('/posts/:postId/comments', comments.getComments);
+router.post('/posts/:postId/comments', comments.addComment);
+router.put('/posts/:postId/comments/:commentId', comments.updateComment);
+router.delete('/posts/:postId/comments/:commentId', comments.removeComment);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,38 +8,7 @@ store = require('./models/post')
 let app = express()
 app.use(bodyParser.json())
 app.use(logger('dev'))
-
-app.get('/posts', (req, res) => {
-  routes.posts.getPosts(req, res)
-})
-
-app.post('/posts', (req, res) => {
-  routes.posts.addPost(req, res)
-})
-
-app.put('/posts/:id', (req, res) => {
-  routes.posts.updatePost(req, res)
-})
-
-app.delete('/posts/:id', (req, res) => {
-  routes.posts.removePost(req, res)
-})
-
-app.get('/posts/:postId/comments', (req, res) =>
-    routes.comments.getComments(req, res)
-)
-
-app.post('/posts/:postId/comments', (req, res) => {
-  routes.comments.addComment(req, res)
-})
-
-app.put('/posts/:postId/comments/:commentId', (req, res) => {
-  routes.comments.updateComment(req, res)
-})
-
-app.delete('/posts/:postId/comments/:commentId', (req, res) => {
-  routes.comments.removeComment(req, res)
-})
+app.use('/', routes);
 
 const server = app.listen(3000)
 module.exports = {app: app, server: server} // for testing
